9bce5486d5b52db3e1caa0325ade2c0751530363
Vladimir Spevak <vladimirspevak1@gmail.com>
2026-07-15 21:56:36 +0200

    Update portal title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Indevo Customer Portal",
+  title: "INDEVO | Customer Portal",
   description: "Customer order tracking portal",
 };
 
